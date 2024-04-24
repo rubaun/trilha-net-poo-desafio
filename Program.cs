71e5c7d1c54553e161ee81d4ad358c56a3082a6f
Ruben Prado <43066684+rubaun@users.noreply.github.com>
2024-04-24 18:36:22 -0300

    Testes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia = new Nokia("222-2222", "Nk01", "123456789", 4);
+Iphone iphone= new Iphone("333-3333", "IP02", "987654321", 6);
+
+nokia.InstalarAplicativo("Instagram");
+
+iphone.InstalarAplicativo("Garage Band");
